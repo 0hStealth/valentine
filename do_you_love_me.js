69fe186e6ad96166dd2e4585277e0902aef1c1bd
0hStealth <adinadhikar@gmail.com>
2025-02-12 17:51:53 +0600

    Valentine

--- do_you_love_me.js
+++ do_you_love_me.js
@@ -5,7 +5,7 @@ const heartLoader = document.querySelector(".cssload-main");
 const yesBtn = document.querySelector(".js-yes-btn");
 const noBtn = document.querySelector(".js-no-btn");
 
-// /change the postion of no button
+
 noBtn.addEventListener("mouseover", () => {
   const newX = Math.floor(Math.random() * questionContainer.offsetWidth);
   const newY = Math.floor(Math.random() * questionContainer.offsetWidth);
